c238f4e6270f318f04faa980685df430909b5e89
picouti <134140295+picouti@users.noreply.github.com>
2025-07-20 17:16:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ← ✅ ajoute cette ligne
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
